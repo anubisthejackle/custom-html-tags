f46e4941b6b0de578c7c6c66c6a28037464a61f5
Travis Weston <tweston2013@gmail.com>
2021-03-13 22:16:08 -0500

    Add ARIA via JS

---
+++ src/index.js
@@ -0,0 +1,31 @@
+function ready(fn) {
+    if (document.readyState != 'loading'){
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+ready(function() {
+
+    let paragraphs = document.getElementsByTagName('content-paragraph');
+    for(let graph of paragraphs) {
+        graph.setAttribute("role", "article");
+    };
+
+    let headers = document.getElementsByTagName('content-header');
+    for(let header of headers) {
+        header.setAttribute("role", "heading");
+    };
+
+    let mainMenus = document.getElementsByTagName('main-menu');
+    for(let menu of mainMenus) {
+        menu.setAttribute("role","navigation");
+    };
+
+    let logos = document.getElementsByTagName('site-logo');
+    for(let logo of logos) {
+        logo.setAttribute("role", "img");
+    };
+
+});
