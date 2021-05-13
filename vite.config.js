fa0eb6a9e47137aea540a329780d8522d1f61854
Travis Weston <tweston2013@gmail.com>
2021-05-13 08:14:36 -0400

    Add HTML minify plugin to build pipeline

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
+import { minifyHtml } from 'vite-plugin-html';
+
 export default {
     base: "/custom-html-tags/",
     build: {
         outDir: "./docs"
-    }
+    },
+    plugins: [
+        minifyHtml(),
+    ]
 }
